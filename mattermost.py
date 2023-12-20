981d30a17728714cdcea900229bae3f810a8f57b
levisingularity <rafael.levi@singularitynet.io>
2023-12-20 15:40:34 -0300

    feat: update action.yml

--- mattermost.py
+++ mattermost.py
@@ -2,14 +2,16 @@ import json
 import requests
 import sys
 
+
 def read_message(file_name):
     try:
-        with open(file_name, 'r') as file:
+        with open(file_name, "r") as file:
             return json.load(file)
     except FileNotFoundError:
         log_error(f"Missing {file_name} file, a previous action should populate it.")
     except Exception as e:
         log_error(f"Error reading message: {e}")
+
 
 def send(webhook, msg):
     try:
@@ -18,10 +20,11 @@ def send(webhook, msg):
     except requests.exceptions.RequestException as e:
         log_error(f"Error sending message: {e}")
 
+
 def is_empty(msg):
     return not bool(msg)
+
 
 def log_error(message):
     print(message, file=sys.stderr)
     sys.exit(1)
-
